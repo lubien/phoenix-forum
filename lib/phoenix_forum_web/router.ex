16e60349e4af9dc638bf7d549de635a47537801b
lubien <lubien1996@gmail.com>
2021-05-02 17:58:53 -0300

    Remove useless routes

--- lib/phoenix_forum_web/router.ex
+++ lib/phoenix_forum_web/router.ex
@@ -24,13 +24,6 @@ defmodule PhoenixForumWeb.Router do
     live "/threads/:id", ThreadLive.Show, :show
     live "/threads/:id/comment/:comment_id", ThreadLive.Show, :edit_comment
     live "/threads/:id/show/edit", ThreadLive.Show, :edit
-
-    live "/comments", CommentLive.Index, :index
-    live "/comments/new", CommentLive.Index, :new
-    live "/comments/:id/edit", CommentLive.Index, :edit
-
-    live "/comments/:id", CommentLive.Show, :show
-    live "/comments/:id/show/edit", CommentLive.Show, :edit
   end
 
   # Other scopes may use custom stacks.
