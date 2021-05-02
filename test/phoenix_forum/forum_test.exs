3923c4276636a7b14dd3000841ddb0e02c1ad76d
lubien <lubien1996@gmail.com>
2021-05-02 18:59:22 -0300

    Rescue forum_test.exs

--- test/phoenix_forum/forum_test.exs
+++ test/phoenix_forum/forum_test.exs
@@ -2,9 +2,9 @@ defmodule PhoenixForum.ForumTest do
   use PhoenixForum.DataCase
 
   alias PhoenixForum.Forum
+  alias PhoenixForum.Forum.Thread
 
   describe "threads" do
-    alias PhoenixForum.Forum.Thread
 
     @valid_attrs %{author: "some author", content: "some content", title: "some title"}
     @update_attrs %{author: "some updated author", content: "some updated content", title: "some updated title"}
@@ -74,26 +74,31 @@ defmodule PhoenixForum.ForumTest do
     @invalid_attrs %{author: nil, content: nil}
 
     def comment_fixture(attrs \\ %{}) do
+      thread = thread_fixture()
+
       {:ok, comment} =
         attrs
+        |> Map.put(:thread_id, thread.id)
         |> Enum.into(@valid_attrs)
         |> Forum.create_comment()
 
-      comment
+      {thread, comment}
     end
 
     test "list_comments/0 returns all comments" do
-      comment = comment_fixture()
+      {_thread, comment} = comment_fixture()
       assert Forum.list_comments() == [comment]
     end
 
     test "get_comment!/1 returns the comment with given id" do
-      comment = comment_fixture()
+      {_thread, comment} = comment_fixture()
       assert Forum.get_comment!(comment.id) == comment
     end
 
     test "create_comment/1 with valid data creates a comment" do
-      assert {:ok, %Comment{} = comment} = Forum.create_comment(@valid_attrs)
+      thread = thread_fixture()
+      attrs = Map.put(@valid_attrs, :thread_id, thread.id)
+      assert {:ok, %Comment{} = comment} = Forum.create_comment(attrs)
       assert comment.author == "some author"
       assert comment.content == "some content"
     end
@@ -103,26 +108,26 @@ defmodule PhoenixForum.ForumTest do
     end
 
     test "update_comment/2 with valid data updates the comment" do
-      comment = comment_fixture()
+      {_thread, comment} = comment_fixture()
       assert {:ok, %Comment{} = comment} = Forum.update_comment(comment, @update_attrs)
       assert comment.author == "some updated author"
       assert comment.content == "some updated content"
     end
 
     test "update_comment/2 with invalid data returns error changeset" do
-      comment = comment_fixture()
+      {_thread, comment} = comment_fixture()
       assert {:error, %Ecto.Changeset{}} = Forum.update_comment(comment, @invalid_attrs)
       assert comment == Forum.get_comment!(comment.id)
     end
 
     test "delete_comment/1 deletes the comment" do
-      comment = comment_fixture()
+      {_thread, comment} = comment_fixture()
       assert {:ok, %Comment{}} = Forum.delete_comment(comment)
       assert_raise Ecto.NoResultsError, fn -> Forum.get_comment!(comment.id) end
     end
 
     test "change_comment/1 returns a comment changeset" do
-      comment = comment_fixture()
+      {_thread, comment} = comment_fixture()
       assert %Ecto.Changeset{} = Forum.change_comment(comment)
     end
   end
